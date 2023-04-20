74ecf1d13d1c85c21d550411b03f83d7f203e473
강병용 <byeongyong.kang@vuno.co>
2023-04-20 10:51:34 +0900

    add files

---
+++ client/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { trpc } from "./trpc";
+
+const App = () => {
+  const [name, setName] = useState("");
+
+  const { data, isLoading, refetch } = trpc.user.getUsers.useQuery();
+
+  const mutation = trpc.user.createUser.useMutation({
+    onSuccess: () => refetch(),
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setName("");
+    mutation.mutate({ name });
+    event.preventDefault();
+  };
+
+  if (isLoading) return <span>Loading ...</span>;
+
+  return (
+    <div>
+      <ul>
+        {(data ?? []).map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" value={name} onChange={handleChange} />
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { trpc, trpcClient } from "./trpc";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </trpc.Provider>
+);
